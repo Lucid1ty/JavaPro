de85a2d160c942fcc071489994e85aadb8fd42a3
Lucidity <2993514952@qq.com>
2022-05-13 23:59:41 +0800

    add StaticFieldDemo1.java and User.java delete Test.java

---
+++ day01-oop-demo/src/com/itheima/d1_static/StaticFieldDemo1.java
@@ -0,0 +1,11 @@
+package com.itheima.d1_static;
+
+public class StaticFieldDemo1 {
+    public static void main(String[] args) {
+        //理解static修饰成员变量的作用和访问特点
+        //1.通过：类名.静态成员变量 的方法访问
+        System.out.println(User.onlineNumber);
+
+    }
+
+}
---
+++ day01-oop-demo/src/com/itheima/d1_static/User.java
@@ -0,0 +1,15 @@
+package com.itheima.d1_static;
+
+public class User {
+    /*
+        在线人数
+        注意:static修饰的成员变量:静态成员变量,只在内存中有一份,可以被共享
+     */
+    public static int onlineNumber = 161;
+
+
+
+
+
+
+}
