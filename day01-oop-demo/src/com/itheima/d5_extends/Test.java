214da166717c9dea17168430376f3507d2348dcf
Lucidity <2993514952@qq.com>
2022-05-17 20:46:12 +0800

    add some code

---
+++ day01-oop-demo/src/com/itheima/d5_extends/Test.java
@@ -0,0 +1,9 @@
+package com.itheima.d5_extends;
+
+public class Test {
+    public static void main(String[] args) {
+        //目标：认清楚继承这种关系，搞清楚使用继承的好处:提高代码的复用性
+        Student s = new Student();
+        s.run();
+    }
+}
