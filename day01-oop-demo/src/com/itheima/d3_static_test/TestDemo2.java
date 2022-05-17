9b1ba4bdd129b7781546ec046298bd27fa5acb08
Lucidity <2993514952@qq.com>
2022-05-17 19:02:23 +0800

    add ArrayUtil.java and TestDemo2.java

---
+++ day01-oop-demo/src/com/itheima/d3_static_test/TestDemo2.java
@@ -0,0 +1,13 @@
+package com.itheima.d3_static_test;
+
+public class TestDemo2 {
+    public static void main(String[] args) {
+        int[] arr = null;
+        int[] arr1 = {};
+        int[] arr2 = {12, 23, 44, 99};
+
+        System.out.println(ArrayUtil.toString(arr));
+        System.out.println(ArrayUtil.toString(arr1));
+        System.out.println(ArrayUtil.toString(arr2));
+    }
+}
