3f74438ede954f66b08516a69cf2614c64fa0d58
Lucidity <2993514952@qq.com>
2022-05-17 20:39:55 +0800

    add some code

---
+++ day01-oop-demo/src/com/itheima/d4_static_singleinstance/SingleInstance2.java
@@ -0,0 +1,29 @@
+package com.itheima.d4_static_singleinstance;
+
+/**
+ * 懒汉单例
+ */
+public class SingleInstance2 {
+
+    /**
+     * 2.定义一个静态的成员变量负责存储一个对象
+     * 只加载一次，只有一份
+     * 注意：最好私有化，避免给别人挖坑
+     */
+    private static SingleInstance2 instance;
+
+    /**
+     * 提供一个方法，对外返回单例对象
+     */
+    public static SingleInstance2 getInstance(){
+        if (instance == null){
+            //1.第一次来拿对象，此时需要创建对象
+            instance = new SingleInstance2();
+        }
+        return instance;
+    }
+
+    //私有化构造器
+    private SingleInstance2(){
+    }
+}
