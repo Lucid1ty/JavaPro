64c69164953109e680f027a03c8c82ad1fb3f78a
Lucidity <2993514952@qq.com>
2022-05-17 20:13:23 +0800

    add some code

---
+++ day01-oop-demo/src/com/itheima/d4_static_singleinstance/Test1.java
@@ -0,0 +1,9 @@
+package com.itheima.d4_static_singleinstance;
+
+public class Test1 {
+    public static void main(String[] args) {
+        //目标：理解饿汉单例的设计步骤
+        SingleInstance s1 = SingleInstance.instance;
+        SingleInstance s2 = SingleInstance.instance;
+    }
+}
