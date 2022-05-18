4269f8be99a3c092c5d2f59a14ab89491d75b26f
Lucidity <2993514952@qq.com>
2022-05-18 20:21:23 +0800

    add some code

---
+++ day02_oop_demo/src/com/itheima/d4_constant/ConstantDemo1.java
@@ -0,0 +1,27 @@
+package com.itheima.d4_constant;
+
+/**
+ * 学会常量的使用，并理解常量
+ */
+public class ConstantDemo1 {
+    public static final String SCHOOL_NAME = "xx教育";
+    public static final String USER_NAME = "admin";
+    public static final String PASS_WORD = "123456";
+
+
+    public static void main(String[] args) {
+        System.out.println(SCHOOL_NAME);
+        System.out.println(SCHOOL_NAME);
+        System.out.println(SCHOOL_NAME);
+        System.out.println(SCHOOL_NAME);
+        System.out.println(USER_NAME);
+        System.out.println(USER_NAME);
+        System.out.println(USER_NAME);
+        System.out.println(USER_NAME);
+        System.out.println(PASS_WORD);
+        System.out.println(PASS_WORD);
+        System.out.println(PASS_WORD);
+        System.out.println(PASS_WORD);
+        System.out.println(PASS_WORD);
+    }
+}
