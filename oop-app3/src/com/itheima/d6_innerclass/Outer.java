359c93a1516985805f157ecc7cd863219cb0c6df
Lucidity <2993514952@qq.com>
2022-06-13 21:47:33 +0800

    add some code

--- oop-app3/src/com/itheima/d6_innerclass/Outer.java
+++ oop-app3/src/com/itheima/d6_innerclass/Outer.java
@@ -2,4 +2,36 @@ package com.itheima.d6_innerclass;
 
 // 外部类
 public class Outer {
+    // 成员内部类：不能加static修饰 属于外部类对象
+    public class Inner{
+        private String name;
+        private int age;
+        public static int a = 100;    // JDK16开始支持在内部类中定义静态成员
+
+        // 静态方法也可以定义
+        public static void test(){
+            System.out.println("test()方法被调用了");
+            System.out.println("内部类中的静态成员a：" + a);
+        }
+        // 普通方法也可以定义
+        public void show(){
+            System.out.println("名称：" + name);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
 }
--- oop-app3/src/com/itheima/d6_innerclass/Test.java
+++ oop-app3/src/com/itheima/d6_innerclass/Test.java
@@ -2,6 +2,10 @@ package com.itheima.d6_innerclass;
 
 public class Test {
     public static void main(String[] args) {
-
+        // 创建内部类(无static修饰，内部类被认为是一个对象)对象,通过new.new的方法创建
+        Outer.Inner in = new Outer().new Inner();
+        in.setName("小明");
+        in.show();
+        Outer.Inner.test();
     }
 }
