9fccd7e6a05b18c54cb11f7b54a61e2964bea76b
Lucidity <2993514952@qq.com>
2022-06-13 21:34:05 +0800

    add some code

--- oop-app3/src/com/itheima/d5_innerclass_static/Outer.java
+++ oop-app3/src/com/itheima/d5_innerclass_static/Outer.java
@@ -2,31 +2,39 @@ package com.itheima.d5_innerclass_static;
 
 //外部类
 public class Outer {
-    //静态成员内部类（和普通类没区别）
+    // 内部类可以直接访问外部类的公开静态成员变量a
     public static int a = 100;
-    //不能访问实例成员
+    // 内部类不能直接访问实例成员hobby
     private String hobby;
+    // 创建内部类
     public static class Inner{
-        private int age;
+        // 内部类
+        // 静态成员内部类(和普通类没区别,只是位置在里面)
         private String name;
+        private int age;
         public static String schoolName;
 
+        // 内部类的show()方法
         public void show(){
+            System.out.println("正在调用show()方法==================");
             System.out.println("名称：" + name);
-            //可以访问外部类的公开静态成员变量
-            System.out.println(a);
+            // 可以访问外部类的公开静态成员变量
+            System.out.println("外部类的公开静态成员变量a：" + a);
 //            System.out.println(hobby);    直接访问外部类实例成员变量报错
-            // 只能通过创建对象的方法去访问
+            // 内部类只能通过创建对象的方法去访问外部类实例成员变量
             Outer o = new Outer();
+            System.out.print("内部类通过创建对象的方法访问外部类实例成员变量hobby：");
             System.out.println(o.hobby);
+            System.out.println("show()方法调用完毕==================");
         }
-        public Inner() {
-        }
-
-        public Inner(int age) {
+        // 无参构造器
+        public Inner() {}
+        // 有参构造器
+        public Inner(String name, int age) {
+            this.name = name;
             this.age = age;
         }
-
+        // get set 方法
         public int getAge() {
             return age;
         }
--- oop-app3/src/com/itheima/d5_innerclass_static/Test.java
+++ oop-app3/src/com/itheima/d5_innerclass_static/Test.java
@@ -2,11 +2,11 @@ package com.itheima.d5_innerclass_static;
 
 public class Test {
     public static void main(String[] args) {
-        //创建内部类
-        Outer.Inner in = new Outer.Inner();
+        //创建内部类对象
+        Outer.Inner in = new Outer.Inner(); // 通过“外部类.内部类”来创建
         in.setAge(18);
         in.setName("内部类");
-        System.out.println(in.getAge());
+        System.out.println("age：" + in.getAge());
         in.show();
     }
 }
