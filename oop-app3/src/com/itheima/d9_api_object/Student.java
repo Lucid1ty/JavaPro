7f539244b3d2111a2061f96652e9466619deb7a9
Lucidity <2993514952@qq.com>
2022-06-17 16:32:25 +0800

    add some code

--- oop-app3/src/com/itheima/d9_api_object/Student.java
+++ oop-app3/src/com/itheima/d9_api_object/Student.java
@@ -1,5 +1,7 @@
 package com.itheima.d9_api_object;
 
+import java.util.Objects;
+
 public class Student {
     private String name;
     private char sex;
@@ -70,24 +72,39 @@ public class Student {
      * 自己重写 equals 方法
      * 两个对象中的内容一样则认为是相等的
      */
-    @Override
-    public boolean equals(Object o){
-        // 1.判断传入的 o 是不是学生类型的
-        if (o instanceof Student){
-            // 将 Object 类型的 o 强制转换为 Student 类型
-            Student s2 = (Student) o;
-            // 谁调用这个方法，谁就是 this，字符串比较用 equals
-//            if (this.name.equals(s2.name) && this.sex == s2.sex && this.age == s2.age){
-//                return true;
-//            }else {
-//                return false;
-//            }
-            // 上面这堆逻辑写成一行代码：
-            return this.name.equals(s2.name) && this.sex == s2.sex && this.age == s2.age;
-        }else {
-            // 学生只能和学生比较，否则结果一定是 false
-            return false;
-        }
-    }
+//    @Override
+//    public boolean equals(Object o){
+//        // 1.判断传入的 o 是不是学生类型的
+//        if (o instanceof Student){
+//            // 将 Object 类型的 o 强制转换为 Student 类型
+//            Student s2 = (Student) o;
+//            // 谁调用这个方法，谁就是 this，字符串比较用 equals
+////            if (this.name.equals(s2.name) && this.sex == s2.sex && this.age == s2.age){
+////                return true;
+////            }else {
+////                return false;
+////            }
+//            // 上面这堆逻辑写成一行代码：
+//            return this.name.equals(s2.name) && this.sex == s2.sex && this.age == s2.age;
+//        }else {
+//            // 学生只能和学生比较，否则结果一定是 false
+//            return false;
+//        }
+//    }
 
+
+    // 重写 equals 方法同样可以快捷生成
+    // 右键-->Generate...-->equals() and hashCode()-->一直下一步
+    // 下面这个是自动生成的 equals 重写方法
+    @Override
+    public boolean equals(Object o) {
+        // 判断是不是两个相同的对象进行比较，如果是两个相同的对象，则返回 true
+        if (this == o) return true;
+        // 如果传入的对象为 null 或者 两个对象的类型不一样，返回 false
+        if (o == null || getClass() != o.getClass()) return false;
+        // 强制转换
+        Student student = (Student) o;
+        // 判断内容是否相等
+        return sex == student.sex && age == student.age && Objects.equals(name, student.name);
+    }
 }
--- oop-app3/src/com/itheima/d9_api_object/Test2.java
+++ oop-app3/src/com/itheima/d9_api_object/Test2.java
@@ -8,7 +8,5 @@ public class Test2 {
         // equals 默认比较对象的地址值
         System.out.println(s1.equals(s2));  // 没有重写 equals 时结果是 false，重写后结果为 true
         System.out.println(s1 == s2);  // false，用 == 号效果和用默认的 equals 方法是一样的
-
-
     }
 }
