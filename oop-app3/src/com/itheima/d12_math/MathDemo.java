981b0150b638ce3bbe399fa9519859fb9f4c7d0e
Lucidity <2993514952@qq.com>
2022-06-20 09:14:57 +0800

    add some code

---
+++ oop-app3/src/com/itheima/d12_math/MathDemo.java
@@ -0,0 +1,12 @@
+package com.itheima.d12_math;
+
+
+public class MathDemo {
+    public static void main(String[] args) {
+        // 返回绝对值
+        System.out.println(Math.abs(10.3));
+        System.out.println(Math.abs(-10.3));
+
+
+    }
+}
